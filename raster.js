1f11976d0e6ec9cf6652f4b17df2fc1b31d0ad7a
Yorick <yorick1230@gmail.com>
2020-02-27 16:49:48 +0100

    WIP: fix mobile responsiveness

--- raster.js
+++ raster.js
@@ -1,33 +1,20 @@
 // copied and modified ~ http://jsfiddle.net/roman_murashov/vxdfLxft/
 
 var lastClicked;
-var grid = clickableGrid(10,10,function(el,row,col,i){
-    console.log("You clicked on element:",el);
-    console.log("You clicked on row:",row);
-    console.log("You clicked on col:",col);
-    console.log("You clicked on item #:",i);
-
-    el.className='clicked';
-    if (lastClicked) lastClicked.className='';
-    lastClicked = el;
-});
-
-document.body.appendChild(grid);
+var grid = clickableGrid(15,15);
      
-function clickableGrid( rows, cols, callback ){
+function clickableGrid( rows, cols ){
     var i=0;
-    var grid = document.createElement('table');
-    grid.className = 'table grid'
+    var grid = $("#gridTable")[0];
     for (var r=0;r<rows;++r){
-        var tr = grid.appendChild(document.createElement('tr'));
+        var tr = $("#gridTable").append('<tr>');
         for (var c=0;c<cols;++c){
-            var cell = tr.appendChild(document.createElement('td'));
-            cell.innerHTML = ++i;
-            cell.addEventListener('click',(function(el,r,c,i){
-                return function(){
-                    callback(el,r,c,i);
-                }
-            })(cell,r,c,i),false);
+        	var cell = $('<td name='+i+++'>');
+        	$(tr).append(cell);
+        	console.log(cell);
+        	$(cell).click(function(el){
+                $(el.target).toggleClass('clicked');
+            });
         }
     }
     return grid;
