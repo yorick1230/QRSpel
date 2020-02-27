f8335e083e8369b1ab29be09fe8ffbb66b92753c
Yorick <yorick1230@gmail.com>
2020-02-27 20:36:35 +0100

    Added grid for qr code

--- raster.js
+++ raster.js
@@ -1,15 +1,16 @@
 // copied and modified ~ http://jsfiddle.net/roman_murashov/vxdfLxft/
 
 var lastClicked;
-var grid = clickableGrid(15,15);
+var grid = clickableGrid(18,18);
      
 function clickableGrid( rows, cols ){
     var i=0;
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
     var grid = $("#gridTable")[0];
     for (var r=0;r<rows;++r){
         var tr = $("#gridTable").append('<tr>');
         for (var c=0;c<cols;++c){
-        	var cell = $('<td name='+i+++'>');
+        	var cell = $('<td name='+alphabet[r]+c+'>'+alphabet[r]+(c+1)+'</td>');
         	$(tr).append(cell);
         	console.log(cell);
         	$(cell).click(function(el){
