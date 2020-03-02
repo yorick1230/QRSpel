364fe9d3db0fc0304c5c2ad6689a049ba98c8cd6
Yorick <yorick1230@gmail.com>
2020-03-02 16:31:56 +0100

    Added library for retrieving qr code data and added table for visualization

--- raster.js
+++ raster.js
@@ -1,22 +1,32 @@
 // copied and modified ~ http://jsfiddle.net/roman_murashov/vxdfLxft/
 
-var lastClicked;
-var grid = clickableGrid(18,18);
-     
-function clickableGrid( rows, cols ){
+function clickableGrid(rows, cols, qrMatrix){
+    var showCoords = false;
     var i=0;
+    var alphabetCount = 0;
     var alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
     var grid = $("#gridTable")[0];
     for (var r=0;r<rows;++r){
         var tr = $("#gridTable").append('<tr>');
+        if(r > (rows / 3)){
+            alphabetCount++;
+        }
         for (var c=0;c<cols;++c){
-        	var cell = $('<td name='+alphabet[r]+c+'>'+alphabet[r]+(c+1)+'</td>');
+
+            if(r > (rows/3) && c > (cols / 3) && showCoords){
+        	    var cell = $('<td>'+alphabet[alphabetCount-1]+((c - (cols / 3))+1)+'</td>');
+            }else{
+                var cell = $('<td>&nbsp;&nbsp;</td>');
+            }
+            qrMatrix[i] === 0 ? $(cell).addClass("blackBlock") : $(cell).addClass("whiteBlock");
+
         	$(tr).append(cell);
-        	console.log(cell);
         	$(cell).click(function(el){
                 $(el.target).toggleClass('clicked');
             });
+            i++;
         }
     }
     return grid;
 }
+
