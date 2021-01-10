0b5b9e960a524422a0584219e49a2ac411454624
Yorick <yorick1230@gmail.com>
2021-01-10 18:47:07 +0100

    heroku deploy test

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 var config = {};
 
-config.db = "qrspel";
-config.host = "localhost";
-config.port = "27017";
+config.db = process.env.database;
+config.user = process.env.user;
+config.host = process.env.host;
+config.pass = process.env.pass;
 
-config.dbUrl = `mongodb://${config.host}:${config.port}/${config.db}`;
+config.dbUrl = `mongodb+srv://${config.user}:${config.pass}@${config.host}/${config.db}?retryWrites=true&w=majority`;
+// example from mongodb: mongodb+srv://user:password@host/dbname?retryWrites=true&w=majority
 
 module.exports = config;
